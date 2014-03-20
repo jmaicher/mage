fd3796cee2cba60feda6dbd4ee1fe089bef0d05b
jmaicher <mail@jmaicher.de>
2014-03-20 14:48:21 +0100

    Allow requests to API from different origin (CORS)

--- mage-web/app/controllers/api/application_controller.rb
+++ mage-web/app/controllers/api/application_controller.rb
@@ -1,4 +1,14 @@
 class API::ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   respond_to :json
+
+  after_filter :cors_set_access_control_headers
+  
+private
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = '*'
+  end
+
 end
