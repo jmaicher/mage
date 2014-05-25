38fd8fb3fc348b2b715ae113bbd384aa3de67d86
jmaicher <mail@jmaicher.de>
2014-05-25 17:26:54 +0200

    Add dynamic namespaces for virtual meetings

--- mage/mage-reactive/server.js
+++ mage/mage-reactive/server.js
@@ -30,8 +30,29 @@ app.post(api_base + '/devices/sessions/confirm', function (req, res) {
   } else {
     res.send(400, 'UUID not found');
   }
+});
 
+app.post(api_base + '/messages', function (req, res) {
+  var id = req.params.id,
+      // nsPath[#room]
+      recipient = req.body.recipient,
+      message = req.body.message,
+      ns, matches;
+  
+  if (recipient.indexOf('/') === 0) {
+    matches = recipient.split('#');
+    ns = matches[0];
+    room = matches[1];
+    
+    if (!room) {
+      io.of(ns).emit('message', message);
+      return res.send(200);
+    } else {
+      res.send(400, 'namespace#room is not implemented yet');
+    }
+  }
 
+  res.send(400, 'For now, only namespaces are allowed as recipient');
 });
 
 app.listen(9000);
@@ -68,9 +89,77 @@ io.configure(function () {
   io.set('transports', ['websocket']);
 
   io.set('authorization', function (handshake, callback) {
-    callback(null, true);
+    var nsPath = handshake.query.ns,
+        meeting;
+  
+    if(nsPath && isMeetingPath(nsPath)) {
+      meeting = VirtualMeeting.findOrCreate(nsPath, io);
+      meeting.authorize(handshake, callback);
+      return; 
+    } else {
+      // Accept globally, can still be denied by ns auth (e.g /devices/auth)
+      callback(null, true);
+    }
   });
 });
 
+function matchMeetingPath(path) {
+  return path.match('^\\/meetings\\/(\\d+)$');
+}
 
+function isMeetingPath(path) {
+  return !!matchMeetingPath(path)
+}
+
+function parseMeetingId(path) {
+  return matchMeetingPath(path)[1];
+}
+
+function VirtualMeeting(nsPath, io) {
+  this.nsPath = nsPath;
+  this.ns = undefined;
+
+  this.createSocketNamespace(io);
+}
+
+// Holds all available virtual meetings
+VirtualMeeting.meetings = {};
+
+VirtualMeeting.findOrCreate = function (nsPath, io) {
+  return VirtualMeeting.find(nsPath)
+    || VirtualMeeting.create(nsPath, io);
+}
+
+VirtualMeeting.find = function (nsPath) {
+  return VirtualMeeting.meetings[nsPath];
+}
+
+VirtualMeeting.create = function (nsPath, io) {
+  var meeting = new VirtualMeeting(nsPath, io);
+  VirtualMeeting.meetings[nsPath] = meeting;
+  return meeting;
+}
+
+VirtualMeeting.prototype.authorize = function (handshake, callback) {
+  var identity = handshake.query.identity;
+
+  if(identity) {
+    identity = JSON.parse(identity);
+    io.log.info('Authorizing connection for ' + this.nsPath);
+    io.log.info(identity);
+
+    callback(null, true);
+    return;
+  }
+
+  callback("Sorry dude, no can do!", false);
+}
+
+VirtualMeeting.prototype.createSocketNamespace = function (io) {
+  var self = this;
+   
+  io.log.info('Creating new socket namespace: ' + this.nsPath);
+  this.ns = io.of(this.nsPath);
+  // TODO: Attach listeners
+}
 
